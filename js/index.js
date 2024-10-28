e8b3ddb30dc7996051e5b29242d3d1cb8f8d29fc
Hamodea <mohmadwahbi22@gmail.com>
2024-10-28 21:15:11 +0100

    first commit

--- js/index.js
+++ js/index.js
@@ -1,6 +1,56 @@
-const user1 = prompt("Enter the first number:");
-const user2 = prompt("Enter the secound number:");
 
-function caculate (){
-    return user1 + user2;
-};
+// simpel räknare
+function calculate() {
+    const num1 = parseInt(prompt("Ange första siffran:"));
+    const operator = prompt("Ange operator (+, -, *, /):");
+    const num2 = parseInt(prompt("Ange andra siffran:"));
+
+    let result;
+    if (operator === "+") {
+      result = num1 + num2;
+    } else if (operator === "-") {
+      result = num1 - num2;
+    } else if (operator === "*") {
+      result = num1 * num2;
+    } else if (operator === "/") {
+      result = num1 / num2;
+    } else {
+      alert("Ogiltig operator!");
+      return;
+    }
+
+    alert("Resultatet är: " + result);
+  }
+
+// ny räknare med functions
+
+  function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+
+  function calculater() {
+    const display = document.getElementById('display');
+    display.value = eval(display.value);
+  }
+
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+
+
+  function askUser (){
+    let user = prompt("write en word:")
+    if  (user.length !==1){
+        alert("vänlig ange bara ett tecken")
+    }
+    
+    if (user >= "0" && user <= "9"){
+        alert("teckent är en siffra");
+    }
+    else if ((user >= 'A' && user <= 'Z') || (user >= 'a' && user <= 'z')) {
+        alert("Tecknet är en bokstav.");
+      } else {
+        alert("Tecknet är varken en siffra eller en bokstav.");
+      }
+    
+  }
