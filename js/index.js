de1fbae46144e2994dd735e8204d1f045215db7e
Mohmad&W <42297664+Hamodea@users.noreply.github.com>
2024-10-28 21:16:30 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -37,20 +37,3 @@ function calculate() {
     document.getElementById('display').value = '';
   }
 
-
-  function askUser (){
-    let user = prompt("write en word:")
-    if  (user.length !==1){
-        alert("vänlig ange bara ett tecken")
-    }
-    
-    if (user >= "0" && user <= "9"){
-        alert("teckent är en siffra");
-    }
-    else if ((user >= 'A' && user <= 'Z') || (user >= 'a' && user <= 'z')) {
-        alert("Tecknet är en bokstav.");
-      } else {
-        alert("Tecknet är varken en siffra eller en bokstav.");
-      }
-    
-  }
